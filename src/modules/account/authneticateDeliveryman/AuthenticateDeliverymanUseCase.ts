3bd218c3c12242930beeedecb4338fa48637fdda
Caio Santos <caiosantos@estudiohaus.com>
2023-05-08 17:49:44 -0300

    Commit in haus

---
+++ src/modules/account/authneticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,36 @@
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken"
+import { prisma } from "../../../database/prismaClient"
+
+
+interface IAuthenticateDeliveryman {
+    username: string,
+    password: string
+}
+
+export class AuthenticateDeliverymanUseCase {
+    async execute({username, password} : IAuthenticateDeliveryman) {
+        const deliveryman = await prisma.deliveryman.findFirst({
+            where: {
+                username
+            }
+        })
+
+        if(!deliveryman) {
+            throw new Error("Username or password invalid!")
+        }
+
+        const passwordMatch = await compare(password, deliveryman.password)
+
+        if(!passwordMatch) {
+            throw new Error("Username or password invalid!")
+        }
+
+        const token = sign({username}, "13474f3ed4bb1d5671be1267e2c57753", {
+            subject: deliveryman.id,
+            expiresIn: "1d"
+        })
+
+        return token
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authneticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClienteController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+
 
 const routes = Router()
 
 const createClienteController = new CreateClienteController
+const authenticateClientController = new AuthenticateClientController
+const createDeliverymanController = new CreateDeliverymanController
+const authenticateDeliverymanController = new AuthenticateDeliverymanController
 
+//routes client
 routes.post("/client/", createClienteController.handle)
+
+//routes authenticate
+routes.post("/client/authenticate/", authenticateClientController.handle)
+routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
+
+//routes deliveryman
+routes.post("/deliveryman", createDeliverymanController.handle)
 
 export {routes}
